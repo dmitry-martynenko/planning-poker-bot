8360305abec71a2a77e72b7ebcd427015316d42c
dm-martynenko <dm-martynenko@yandex-team.ru>
2023-03-26 21:37:07 +0300

    add project

---
+++ src/main/kotlin/ru/toxicdev/planningpokerbot/PlanningPokerBotApplication.kt
@@ -0,0 +1,11 @@
+package ru.toxicdev.planningpokerbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlanningPokerBotApplication
+
+fun main(args: Array<String>) {
+	runApplication<PlanningPokerBotApplication>(*args)
+}
---
+++ src/test/kotlin/ru/toxicdev/planningpokerbot/PlanningPokerBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.toxicdev.planningpokerbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlanningPokerBotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
